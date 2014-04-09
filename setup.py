91237979413df0c0bb25fc2a28ca5f98c40dfdeb
Daniel da Silva <daniel@meltingwax.net>
2014-04-09 19:25:25 -0400

    packaging for PIP

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='youtubegen',
+      version='0.9',
+      description='Generate and upload YouTube music videos',
+      license='GPL',
+      keywords='youtube music video',
+      author='Daniel da Silva',
+      author_email='var.mail.daniel@gmail.com',
+      url='https://github.com/ddasilva/youtubegen',
+      packages=['youtubegen'],
+      install_requires=['PIL', 'gdata'],
+      entry_points="""
+      [console_scripts]
+      youtubegen = youtubegen:main
+      """,
+)
+
