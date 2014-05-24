841b4e87fad7022fd6ea4fc04bca800358055f28
Daniel da Silva <daniel@meltingwax.net>
2014-05-24 12:55:11 -0400

    bump to 0.9.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='youtubegen',
-      version='0.9',
+      version='0.9.1',
       description='Generate and upload YouTube music videos',
       license='GPL',
       keywords='youtube music video',
